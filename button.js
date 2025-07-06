7551e2717b2e3d1438ab64a96f1f2d922bf19195
Jrljvb <vieira.laertejr@gmail.com>
2025-07-06 14:37:48 -0300

    Update button.js

--- button.js
+++ button.js
@@ -1,1 +1,10 @@
-// botão fake para build
+export function Button({ children, className, onClick }) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg font-medium ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
