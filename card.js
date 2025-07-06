7f646a198d985fa0991961b7d1a7b16003c3f8cc
Jrljvb <vieira.laertejr@gmail.com>
2025-07-06 14:38:33 -0300

    Update card.js

--- card.js
+++ card.js
@@ -1,1 +1,11 @@
-// card fake para build
+export function Card({ children, className }) {
+  return (
+    <div className={`rounded-xl shadow-lg overflow-hidden ${className}`}>
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children, className }) {
+  return <div className={className}>{children}</div>;
+}
